db4974d72f2463ee6cba9e83b4821bce4390d831
Armando Rodríguez <jarodriguezvera@gmail.com>
2023-05-03 12:49:28 -0600

    Agregar archivos de proyecto.

---
+++ Alumno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Listas_2023_II
+{
+    internal class Alumno
+    {
+        private string nombre;
+        private string numeroCuenta;
+
+        public Alumno(string nombre, string numeroCuenta)
+        {
+            Nombre = nombre;
+            NumeroCuenta = numeroCuenta;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string NumeroCuenta { get => numeroCuenta; set => numeroCuenta = value; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Listas_2023_II
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Alumno> alumnos = new List<Alumno>();
+            alumnos.Add(new Alumno("Jose","40220222"));
+            alumnos.Add(new Alumno("Juan", "90222232"));
+            alumnos.RemoveAt(0);
+
+            foreach(Alumno alumno in alumnos)
+            {
+               Console.WriteLine("Nombre: {0} \n Número de Cuenta: {1} ",alumno.Nombre,alumno.NumeroCuenta);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
